e4e77d74b00f590838887e3f6dd7c17173647215
Utkarsh Shukla <61664827+Utkarsh-Shukla-Github@users.noreply.github.com>
2021-10-07 02:35:25 +0530

    problems/combinations/solution/

---
+++ Combination/O(k*n^k) Time Complexity | O(n) Space Complexity/source.cpp
@@ -0,0 +1,24 @@
+class Solution {
+
+vector<vector<int>> ans;
+    
+private:
+    void recCombination(int idx, int k, vector<int>& current, int n) {
+        if(current.size() == k) {
+            ans.push_back(current);
+            return;
+        }
+        
+        for(int i=idx; i<=n; i++) {
+            current.push_back(i);
+            recCombination(i+1, k, current, n);
+            current.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> current;
+        recCombination(1, k, current, n);
+        return ans;
+    }
+};
