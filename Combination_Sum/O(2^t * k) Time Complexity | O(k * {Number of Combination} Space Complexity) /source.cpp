33cb45842c78ab9f6e241f306bd5d37e965ac28f
Utkarsh Shukla <61664827+Utkarsh-Shukla-Github@users.noreply.github.com>
2021-10-08 15:25:18 +0530

    problems/combination-sum/solution/

---
+++ Combination_Sum/O(2^t * k) Time Complexity | O(k * {Number of Combination} Space Complexity) /source.cpp
@@ -0,0 +1,32 @@
+class Solution {
+
+private: 
+    vector<vector<int>> ans; 
+    void findCombination(int ind, int target, vector<int> &candidates, vector<int>&ds) {
+
+        // base case
+        if(ind == candidates.size()) {
+            if(target == 0) {
+                ans.push_back(ds); 
+            }
+            return; 
+        }
+
+        // pick up the element 
+        if(candidates[ind] <= target) {
+            ds.push_back(candidates[ind]); 
+            findCombination(ind, target - candidates[ind], candidates, ds); 
+            ds.pop_back(); 
+        }
+        
+        findCombination(ind+1, target, candidates, ds); 
+        
+    }
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> ds; 
+        findCombination(0, target, candidates, ds); 
+        return ans; 
+    }
+};
