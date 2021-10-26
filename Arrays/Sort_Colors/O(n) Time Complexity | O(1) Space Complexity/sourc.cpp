9d1f7148a539693c6a032e36de0c14dee7b50f49
Utkarsh Shukla <61664827+Utkarsh-Shukla-Github@users.noreply.github.com>
2021-10-26 13:02:59 +0530

    problems/sort-colors/solution/

---
+++ Arrays/Sort_Colors/O(n) Time Complexity | O(1) Space Complexity/sourc.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        const int n = nums.size();
+        
+        int  zeros = 0, ones = 0, twos = 0;
+        
+        for(int value : nums){
+            if(value == 0) zeros++;
+            if(value == 1) ones++;
+            if(value == 2) twos++;
+        }
+        
+        nums.clear
+            
+        while(zeros--){
+            nums.push_back(0);
+        }
+        
+        while(ones--){
+            nums.push_back(1);
+        }
+        
+        while(twos--){
+            nums.push_back(2);
+        }
+        
+    }
+};
