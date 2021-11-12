88be5bc9143ea4f564b884a97d6e83145e47b182
✪ Utkarsh Shukla ✪ <61664827+Utkarsh-Shukla-Github@users.noreply.github.com>
2021-11-12 12:50:06 +0530

    problems/two-sum/solution/

---
+++ Hashing/2_Sum_Problem.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> sum;
+        vector<int> index;
+        for(int i=0;i<nums.size();i++)
+            sum.push_back({nums[i],i});
+        sort(sum.begin(),sum.end());
+        int i = 0, j = nums.size() - 1;
+        while(i < j) {
+            if(sum[i].first + sum[j].first == target) {
+                index.push_back(sum[i].second);
+                index.push_back(sum[j].second);
+                break;
+            }
+            else if(sum[i].first + sum[j].first < target) {
+                i++;
+            }
+            else j--;
+        }
+        
+        return index;
+    }
+};
