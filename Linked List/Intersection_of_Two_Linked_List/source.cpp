57d87c3de0788f3a0da5cac2874c210a059bece2
__Utkarsh <61664827+Utkarsh-Shukla-Github@users.noreply.github.com>
2021-12-05 17:25:13 +0530

    problems/intersection-of-two-linked-lists/solution

---
+++ Linked List/Intersection_of_Two_Linked_List/source.cpp
@@ -0,0 +1,16 @@
+// O(m+n) Time Complexity | O(1) Space Complexity
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL) return NULL;
+        ListNode* a = headA;        
+        ListNode* b = headB;
+
+        while(a!=b){
+            a = a == NULL ? headB : a -> next;
+            b = b == NULL ? headA : b -> next;
+        }
+        return a;
+    }
+};
